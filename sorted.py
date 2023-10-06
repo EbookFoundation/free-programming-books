e0627ad74e29966515fcae2eacd92c937cde3e4a
farhanahmed01 <124406006+farhanahmed01@users.noreply.github.com>
2023-10-07 03:50:13 +0530

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,9 @@
+# List of course or book names
+courses_or_books = ["Python Programming", "Machine Learning", "Algorithms", "Web Development", "Data Science"]
+
+# Sort the list alphabetically in-place
+courses_or_books.sort()
+
+# Print the sorted list
+for course_or_book in courses_or_books:
+    print(course_or_book)
